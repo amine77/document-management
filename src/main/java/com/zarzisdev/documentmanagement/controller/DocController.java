feeaece4f8235ba7a3da8a27a7d13aa678866ae6
amine77 <charrad.amine@yahoo.fr>
2019-03-04 13:11:18 +0100

    pdf generation + doc details

--- src/main/java/com/zarzisdev/documentmanagement/controller/DocController.java
+++ src/main/java/com/zarzisdev/documentmanagement/controller/DocController.java
@@ -2,13 +2,19 @@ package com.zarzisdev.documentmanagement.controller;
 
 import com.zarzisdev.documentmanagement.model.Doc;
 import com.zarzisdev.documentmanagement.repository.DocRepository;
+import com.zarzisdev.documentmanagement.uils.GeneratePdfReport;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 
 @RestController
@@ -18,7 +24,36 @@ public class DocController {
     DocRepository docRepository;
 
     @GetMapping("/documents")
-    public List<Doc> getDoc(){
+    public List<Doc> getDoc() {
         return docRepository.findAll();
+    }
+
+    @GetMapping("/documents/{id}")
+    public ResponseEntity<Doc> getDocDetails(@PathVariable("id") Long id) {
+        Optional<Doc> docData = docRepository.findById(id);
+        if (docData.isPresent()) {
+            return new ResponseEntity<>(docData.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+    }
+
+    @RequestMapping(value = "/pdf", method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_PDF_VALUE)
+    public ResponseEntity<InputStreamResource>docsReport() throws IOException {
+
+        List<Doc> docs = (List<Doc>) docRepository.findAll();
+
+        ByteArrayInputStream bis = GeneratePdfReport.docsReport(docs);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Disposition", "inline; filename=docsreport.pdf");
+
+        return ResponseEntity
+                .ok()
+                .headers(headers)
+                .contentType(MediaType.APPLICATION_PDF)
+                .body(new InputStreamResource(bis));
     }
 }
---
+++ src/main/java/com/zarzisdev/documentmanagement/uils/GeneratePdfReport.java
@@ -0,0 +1,78 @@
+package com.zarzisdev.documentmanagement.uils;
+
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.zarzisdev.documentmanagement.model.Doc;
+
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class GeneratePdfReport {
+    public static ByteArrayInputStream docsReport(List<Doc> docs) {
+
+        Document document = new Document();
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        try {
+
+            PdfPTable table = new PdfPTable(3);
+            table.setWidthPercentage(60);
+            table.setWidths(new int[]{1, 3, 3});
+
+            Font headFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
+
+            PdfPCell hcell;
+            hcell = new PdfPCell(new Phrase("Id", headFont));
+            hcell.setHorizontalAlignment(Element.ALIGN_CENTER);
+            table.addCell(hcell);
+
+            hcell = new PdfPCell(new Phrase("Name", headFont));
+            hcell.setHorizontalAlignment(Element.ALIGN_CENTER);
+            table.addCell(hcell);
+
+            hcell = new PdfPCell(new Phrase("Population", headFont));
+            hcell.setHorizontalAlignment(Element.ALIGN_CENTER);
+            table.addCell(hcell);
+
+            for (Doc doc : docs) {
+
+                PdfPCell cell;
+
+                cell = new PdfPCell(new Phrase(doc.getId().toString()));
+                cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+                table.addCell(cell);
+
+                cell = new PdfPCell(new Phrase(doc.getAuthor()));
+                cell.setPaddingLeft(5);
+                cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                cell.setHorizontalAlignment(Element.ALIGN_LEFT);
+                table.addCell(cell);
+
+                cell = new PdfPCell(new Phrase(String.valueOf(doc.getTitle())));
+                cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                cell.setHorizontalAlignment(Element.ALIGN_RIGHT);
+                cell.setPaddingRight(5);
+                table.addCell(cell);
+            }
+
+            PdfWriter.getInstance(document, out);
+            document.open();
+            document.add(table);
+
+            document.close();
+
+        } catch (DocumentException ex) {
+
+            Logger.getLogger(GeneratePdfReport.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return new ByteArrayInputStream(out.toByteArray());
+    }
+}
